05df34fe528221edcd5e4d77da2db0f0f4ef8203
Claude-Mops47 <mopenobiaclaude@gmail.com>
2022-11-07 21:42:01 +0100

    Add a Class and methods

--- cap.py
+++ cap.py
@@ -20,3 +20,27 @@ array_2 = [19, -20, -4.7, 6, 9, 42]
 array_3 = [4, 0.3, -9, 8, 6, 14]
 
 closest(array_3)
+
+# Class and methods
+class Person:
+    def __init__(self, name, sex, profession) -> None:
+        # data members (instance variable)
+        self.name = name
+        self.sex = sex
+        self.profession = profession
+
+    # Behavior (instance methods)
+    def show(self):
+        print('Name:', self.name, 'Sex:', self.sex, 'Profession:', self.profession)
+
+    # Behavior (instance methods)
+    def work(self):
+        print(self.name, 'working as a', self.profession)
+
+
+# Create object of a class
+person = Person('Mops', 'Male', 'Software Engineer')
+
+# Call methods
+person.show()
+person.work()
