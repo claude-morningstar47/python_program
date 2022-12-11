a5699e987172e624b0fd323ea0c383dd0458d055
Claude-Mops47 <mopenobiaclaude@gmail.com>
2022-12-11 20:37:08 +0100

    min_number

---
+++ min_number.py
@@ -0,0 +1,14 @@
+# define a list of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# initialize the minimum number to the first number in the list
+min_num = numbers[0]
+
+# iterate over the numbers in the list
+for num in numbers:
+  # if the current number is less than the current minimum, update the minimum
+  if num < min_num:
+    min_num = num
+
+# print the minimum number
+print(min_num)
