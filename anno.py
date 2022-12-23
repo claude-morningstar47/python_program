d48883dcf639cfccee5f421b31c61a9b838f5694
Claude-Mops47 <mopenobiaclaude@gmail.com>
2022-12-23 12:33:57 +0100

    ok python

--- anno.py
+++ anno.py
@@ -1,25 +1,25 @@
-import random
+# import random
 
-import pyautogui as pg
+# import pyautogui as pg
 
-import time
+# import time
 
-animal =("monkey","donkey", "dog")
+# animal =("monkey","donkey", "dog")
 
-time.sleep(8)
+# time.sleep(8)
 
-for i in range(23):
-    a = random.choice(animal)
-    pg.write("you are a" + a)
-    pg.press('enter')
+# for i in range(23):
+#     a = random.choice(animal)
+#     pg.write("you are a" + a)
+#     pg.press('enter')
     
 
 #! Single number
 def single_number():
     count = 0
     for i in nums:
-        count = count * i
+        count = count =+i
         return count
 
-nums = [4,5,3,1,2]
+nums = [4,3,5,1,2]
 print(single_number())
