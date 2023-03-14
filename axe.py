087126fdbff85a9a4d73897012c27343adf0bd11
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-03-14 11:52:11 +0100

    mysql

--- axe.py
+++ axe.py
@@ -1,13 +1,19 @@
 # Axe
+import mysql.connector
+
+
 def axe(_x, _y):
     for y in range(_y):
         for x in range(_x):
             print(" #_#" * 10, end="")
     print()
 
-axe(1,1)
+
+axe(1, 1)
 
 # Pyramid
+
+
 def pattern(rows):
     k = 2 * rows - 2
     for i in range(0, rows):
@@ -18,26 +24,37 @@ def pattern(rows):
             print("*", end=" ")
         print()
 
-pattern(8)
+# pattern(8)
 
-# number
-def power(n1, n2):
-    return n1, n2
-
-
-base = int(input("Enter base number: "))
-a = int(input("Enter power number(positive integer): "))
-
-result = power(base,a)
-print(base, "^", a,"= ", result)
-
-if (a != 0):
-    base * power(base, a -1 )
-else:
-    pass
-
-print(base,"^", a,"=", result)
-print(base,"^", a,"=", result)
-print(base,"^", a,"=", result)
+# # number
+# def power(n1, n2):
+#     return n1, n2
 
 
+# base = int(input("Enter base number: "))
+# a = int(input("Enter power number(positive integer): "))
+
+# result = power(base,a)
+# print(base, "^", a,"= ", result)
+
+# if (a != 0):
+#     base * power(base, a -1 )
+# else:
+#     pass
+
+# print(base,"^", a,"=", result)
+# print(base,"^", a,"=", result)
+# print(base,"^", a,"=", result)
+
+
+db = mysql.connector.connect(host='localhost',
+                             user='root',
+                             password='Secret Key',
+                             db='sso')
+cdb = db.cursor()
+cdb.execute('SELECT * FROM users')
+
+for row in cdb.fetchall():
+    print(row)
+
+db.close()
