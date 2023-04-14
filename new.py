02abb205c7c322b35f09d573b396cc66c23b71a7
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-04-14 12:52:23 +0000

    ok

--- new.py
+++ new.py
@@ -1,8 +1,8 @@
 import time
 
-a = " HELLO je suis le chatiment"
+a = " HELLO MORNINGSTAR, Notez que le code JavaScript utilise setTimeout pour simuler l'effet de la fonction Sleep de Windows.h. De plus, il utilise process.stdout.write plutôt que console.log pour permettre l'impression des caractères sur la même ligne. "
 n = len(a)
 
 for i in range(n):
-    time.sleep(1000)
-    print(a[i], end="")
+    time.sleep(0.1)
+    print(a[i], end="", flush=True)
